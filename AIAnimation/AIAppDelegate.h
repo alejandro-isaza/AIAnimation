6ea1d20c36cc5f2a3f4df33183b5cd7eb9cb17a1
Alejandro Isaza <alejandro.isaza@gmail.com>
2013-03-26 17:29:46 -0700

    Initial Commit

---
+++ AIAnimation/AIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AIAppDelegate.h
+//  AIAnimation
+//
+//  Created by Alejandro Isaza on 2013-03-26.
+//  Copyright (c) 2013 Alejandro Isaza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AIAnimation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AIAnimation
+//
+//  Created by Alejandro Isaza on 2013-03-26.
+//  Copyright (c) 2013 Alejandro Isaza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AIAppDelegate class]));
+    }
+}
