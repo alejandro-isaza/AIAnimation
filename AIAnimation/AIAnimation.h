429a449159e15d8cd19eb70dae0e5f2a63c8038e
Alejandro Isaza <alejandro.isaza@gmail.com>
2013-05-13 20:15:59 -0700

    Fix missing import

--- AIAnimation/AIAnimation.h
+++ AIAnimation/AIAnimation.h
@@ -1,6 +1,8 @@
 //  Created by Alejandro Isaza on 2013-03-26.
 //  Copyright (c) 2013 Alejandro Isaza. All rights reserved.
 
+#import <UIKit/UIKit.h>
+
 
 @interface AIAnimation : NSObject <NSCopying>
 
